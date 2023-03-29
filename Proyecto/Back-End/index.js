f395cf85be09c05ec194624d63c16a80b8dda388
Dado00 <dado061998@gmail.com>
2023-03-29 15:28:54 -0600

    Endpoint proyectos

--- Proyecto/Back-End/index.js
+++ Proyecto/Back-End/index.js
@@ -49,5 +49,55 @@ app.post('/register', (req, res) => {
 	
 });
 
+app.get('/proyectos', (req, res) => {
+    connection.query('SELECT * FROM proyectos', (err, rows, fields) => {
+      if (!err)
+        res.send(rows);
+      else
+        console.log(err);
+    })
+  });
+
+  app.get('/proyectos/:id', (req, res) => {
+    connection.query('SELECT * FROM proyectos WHERE id = ?', [req.params.id], (err, rows, fields) => {
+      if (!err)
+        res.send(rows);
+      else
+        console.log(err);
+    })
+  });
+
+  app.post('/proyectos', (req, res) => {
+    let proyecto = req.body;
+    var sql = "INSERT INTO proyectos (nombre, descripcion, materia, fecha_entrega, usuario_asignado) VALUES (?, ?, ?, ?, ?)";
+    connection.query(sql, [proyecto.nombre, proyecto.descripcion, proyecto.materia, proyecto.fecha_entrega, proyecto.usuario_asignado], (err, rows, fields) => {
+      if (!err)
+        res.send("Proyecto agregado exitosamente.");
+      else
+        console.log(err);
+    })
+  });
+
+  app.put('/proyectos/:id', (req, res) => {
+    let proyecto = req.body;
+    var sql = "UPDATE proyectos SET nombre = ?, descripcion = ?, materia = ?, fecha_entrega = ?, usuario_asignado = ? WHERE id = ?";
+    connection.query(sql, [proyecto.nombre, proyecto.descripcion, proyecto.materia, proyecto.fecha_entrega, proyecto.usuario_asignado, req.params.id], (err, rows, fields) => {
+      if (!err)
+        res.send("Proyecto actualizado exitosamente.");
+      else
+        console.log(err);
+    })
+  });
+
+  app.delete('/proyectos/:id', (req, res) => {
+    connection.query('DELETE FROM proyectos WHERE id = ?', [req.params.id], (err, rows, fields) => {
+      if (!err)
+        res.send("Proyecto eliminado exitosamente.");
+      else
+        console.log(err);
+    })
+  });
+
+
 app.listen(puerto, () => console.log(`app listening on http://localhost:${puerto}`));
 
