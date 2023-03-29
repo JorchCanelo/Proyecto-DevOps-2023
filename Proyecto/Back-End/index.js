0bc4578ab8016d15704cb37805fb62868d758333
Kanin <77802064+Kanin-020@users.noreply.github.com>
2023-03-28 23:06:03 -0600

    Cambios  la estructura del backed

--- Proyecto/Back-End/index.js
+++ Proyecto/Back-End/index.js
@@ -1,18 +1,21 @@
 const express = require('express');
-const app = express();
+const dotenv = require('dotenv');
+const jwt = require('jsonwebtoken');
+const bodyParser = require('body-parser');
+const mysql = require('mysql');
+const session = require('express-session');
+const connection = require('./dbConnection');
 
+const app = express();
 const puerto = process.env.PORT || 5000;
 
-var estudiantes = [
-	{nombre:'Jesus', matricula:'a16001674'},
-	{nombre:'Juan', matricula:'a16001675'},
-	{nombre:'Pedro', matricula:'a16001676'},
-];
+app.use(express.urlencoded({ extended: false }));
+app.use(express.json());
 
-app.get('/estudiantes', (req, res) =>
-	res.json(estudiantes)
-);
+app.use(session({
+  secret: 'secret',
+  resave: 'true',
+  saveUnitialized: 'true',
+}));
 
 app.listen(puerto, () => console.log(`app listening on http://localhost:${puerto}`) );
-
-module.exports = app;
