6c33409c1dde6aa0079fec31970d705c0c1e1680
Jorge Alberto Chí León <46463280+JorchCanelo@users.noreply.github.com>
2023-04-25 17:43:38 -0600

    Respuestas cambiadas a json

--- App/Presentation/Routes/TaskManager.js
+++ App/Presentation/Routes/TaskManager.js
@@ -8,18 +8,18 @@ const authorizer = require('../../DataAccess/Authorizer');
 router.get('/tareas', authorizer.verificarToken, (req, res) => {
 	connection.query('SELECT * FROM tareas', (err, rows, fields) => {
 		if (!err)
-			res.send(rows);
+			res.json({ rows });
 		else
-			console.log(err);
+			res.status(500).json({ error });
 	})
 });
 
 router.get('/tareas/:id', authorizer.verificarToken, (req, res) => {
 	connection.query('SELECT * FROM tareas WHERE id = ?', [req.params.id], (err, rows, fields) => {
 		if (!err)
-			res.send(rows);
+			res.json({ rows });
 		else
-			console.log(err);
+			res.status(500).json({ error });
 	})
 });
 
@@ -28,9 +28,9 @@ router.post('/tareas', authorizer.verificarToken, (req, res) => {
 	var sql = "INSERT INTO tareas (nombre, descripcion, estado, fecha_entrega, proyecto_asociado) VALUES (?, ?, ?, ?, ?)";
 	connection.query(sql, [tarea.nombre, tarea.descripcion, tarea.estado, tarea.fecha_entrega, tarea.proyecto_asociado], (err, rows, fields) => {
 		if (!err)
-			res.send("Tarea agregada exitosamente.");
+			res.json({ message: 'Tarea agregada exitosamente.' });
 		else
-			console.log(err);
+			res.status(500).json({ error });
 	})
 });
 
@@ -39,18 +39,21 @@ router.put('/tareas/:id', authorizer.verificarToken, (req, res) => {
 	var sql = "UPDATE tareas SET nombre = ?, descripcion = ?, estado = ?, fecha_entrega = ?, proyecto_asociado = ? WHERE id = ?";
 	connection.query(sql, [tarea.nombre, tarea.descripcion, tarea.estado, tarea.fecha_entrega, tarea.proyecto_asociado, req.params.id], (err, rows, fields) => {
 		if (!err)
-			res.send("Tarea actualizada exitosamente.");
+			res.json({ message: 'Tarea actualizada exitosamente.' });
 		else
-			console.log(err);
+			res.status(500).json({ error });
 	})
 });
 
 router.delete('/tareas/:id', authorizer.verificarToken, (req, res) => {
 	connection.query('DELETE FROM tareas WHERE id = ?', [req.params.id], (err, rows, fields) => {
-		if (!err)
-			res.send("Tarea eliminada exitosamente.");
-		else
-			console.log(err);
+		if (error) {
+			res.status(500).json({ error });
+		} else if (results.affectedRows === 0) {
+			res.status(404).json({ message: 'Tarea no encontrada' });
+		} else {
+			res.json({ message: 'Tarea eliminada' });
+		}
 	})
 });
 
