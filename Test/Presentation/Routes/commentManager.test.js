b9f5b5fc24fd8a7d594b9164f5ada71ee4153f23
Kanin <77802064+Kanin-020@users.noreply.github.com>
2023-04-30 13:17:44 -0600

    Prototipo puebas y script de pruebas

--- Test/Presentation/Routes/commentManager.test.js
+++ Test/Presentation/Routes/commentManager.test.js
@@ -1,64 +1,129 @@
-const request = require('../Back-End/node_modules/supertest');
-const router = require('../Back-End/commentManager');
+// Importamos los módulos necesarios
+const request = require('supertest');
+const express = require('express');
+const commentRoutes = require('../../../App/Presentation/Routes/commentManager');
+// const mockConnection = require('../../DataAccess/MockConnection');
+const authorizer = require('../../DataAccess/Authorizer');
 
-describe('Pruebas de rutas de comentarios', () => {
-    it('Debería obtener todos los comentarios', async () => {
-      const response = await request(router)
-        .get('/comentarios')
-        .set('Authorization', `Bearer ${token}`); // token de autenticación
-      expect(response.status).toBe(200);
-      expect(response.body.length).toBeGreaterThan(0);
-    });
-  
-    it('Debería obtener un comentario por id', async () => {
-      const id = 1; // id de un comentario existente
-      const response = await request(router)
-        .get(`/comentarios/${id}`)
-        .set('Authorization', `Bearer ${token}`); // token de autenticación
-      expect(response.status).toBe(200);
-      expect(response.body.length).toBe(1);
-      expect(response.body[0].id).toBe(id);
-    });
-  
-    it('Debería crear un nuevo comentario', async () => {
-      const comentario = {
-        autor: 'Juan',
-        contenido: 'Este es un nuevo comentario',
-        fecha: new Date(),
-        estado: 'activo',
-        tarea_asociada: 1
-      };
-      const response = await request(router)
-        .post('/comentarios')
-        .send(comentario)
-        .set('Authorization', `Bearer ${token}`); // token de autenticación
-      expect(response.status).toBe(200);
-      expect(response.text).toBe('Comentario agregado exitosamente.');
-    });
-  
-    it('Debería actualizar un comentario existente', async () => {
-      const id = 1; // id de un comentario existente
-      const comentario = {
-        autor: 'Pedro',
-        contenido: 'Este es un comentario actualizado',
-        fecha: new Date(),
-        estado: 'inactivo',
-        tarea_asociada: 2
-      };
-      const response = await request(router)
-        .put(`/comentarios/${id}`)
-        .send(comentario)
-        .set('Authorization', `Bearer ${token}`); // token de autenticación
-      expect(response.status).toBe(200);
-      expect(response.text).toBe('Comentario actualizado exitosamente.');
-    });
-  
-    it('Debería eliminar un comentario existente', async () => {
-      const id = 1; // id de un comentario existente
-      const response = await request(router)
-        .delete(`/comentarios/${id}`)
-        .set('Authorization', `Bearer ${token}`); // token de autenticación
-      expect(response.status).toBe(200);
-      expect(response.text).toBe('Comentario eliminado exitosamente.');
-    });
-  });
+const mockConnection = {
+	query: jest.fn()
+};
+
+// Mockeamos la conexión a la base de datos
+jest.mock('../../../App/DataAccess/DBConnection', () => mockConnection);
+
+// Creamos una instancia de express y le agregamos los middlewares y rutas necesarias
+const app = express();
+app.use(express.json());
+app.use(commentRoutes);
+
+describe('Pruebas unitarias para los endpoints de comentarios', () => {
+	let token;
+
+	beforeAll(async () => {
+		token = await authorizer.generarToken();
+	});
+
+	describe('GET /comentarios', () => {
+		test('debería devolver una lista de comentarios', async () => {
+			// Hacemos una petición HTTP a nuestra aplicación utilizando supertest
+			const response = await request(app)
+				.get('/comentarios')
+				.set('Authorization', `Bearer ${token}`)
+				.expect(200);
+
+			// Verificamos que la respuesta contenga una lista de comentarios
+			expect(response.body).toBeInstanceOf(Array);
+			expect(response.body.length).toBeGreaterThan(0);
+		});
+	});
+
+	// Agrega más pruebas unitarias aquí
+});
+
+
+
+
+// const request = require('supertest')
+// const app = require('../../../App/Presentation/Routes/commentManager');
+// const mockConnection = require('../../DataAccess/MockConnection');
+// const authorizer = require('../../DataAccess/Authorizer');
+
+// jest.mock('../../../App/DataAccess/DBConnection', () => mockConnection);
+
+// describe('Pruebas unitarias para los endpoints de comentarios', () => {
+// 	let token;
+
+// 	beforeAll(async () => {
+// 		token = await authorizer.generarToken();
+// 	});
+
+// 	describe('GET /comentarios', () => {
+// 		test('debería devolver una lista de comentarios', async () => {
+// 			const response = await request(app)
+// 				.get('/comentarios')
+// 				.set('Authorization', `Bearer ${token}`)
+// 				.expect(200);
+
+// 			expect(response.body).toHaveLength(3); // Modificar según el número de comentarios que se espera devolver
+// 		});
+// 	});
+
+// 	describe('GET /comentarios/:id', () => {
+// 		test('debería devolver un comentario específico', async () => {
+// 			const response = await request(app)
+// 				.get('/comentarios/1')
+// 				.set('Authorization', `Bearer ${token}`)
+// 				.expect(200);
+
+// 			expect(response.body[0].id).toBe(1); // Modificar según el ID del comentario que se espera devolver
+// 		});
+// 	});
+
+// 	describe('POST /comentarios', () => {
+// 		test('debería agregar un comentario', async () => {
+// 			const response = await request(app)
+// 				.post('/comentarios')
+// 				.set('Authorization', `Bearer ${token}`)
+// 				.send({
+// 					autor: 'Juan',
+// 					contenido: 'Este es un comentario de prueba',
+// 					fecha: '2022-04-01',
+// 					estado: 'pendiente',
+// 					tarea_asociada: 1
+// 				})
+// 				.expect(200);
+
+// 			expect(response.text).toBe('Comentario agregado exitosamente.');
+// 		});
+// 	});
+
+// 	describe('PUT /comentarios/:id', () => {
+// 		test('debería actualizar un comentario existente', async () => {
+// 			const response = await request(app)
+// 				.put('/comentarios/1')
+// 				.set('Authorization', `Bearer ${token}`)
+// 				.send({
+// 					autor: 'Juan',
+// 					contenido: 'Este es un comentario actualizado',
+// 					fecha: '2022-04-01',
+// 					estado: 'completado',
+// 					tarea_asociada: 1
+// 				})
+// 				.expect(200);
+
+// 			expect(response.text).toBe('Comentario actualizado exitosamente.');
+// 		});
+// 	});
+
+// 	describe('DELETE /comentarios/:id', () => {
+// 		test('debería eliminar un comentario existente', async () => {
+// 			const response = await request(app)
+// 				.delete('/comentarios/1')
+// 				.set('Authorization', `Bearer ${token}`)
+// 				.expect(200);
+
+// 			expect(response.text).toBe('Comentario eliminado exitosamente.');
+// 		});
+// 	});
+// });
