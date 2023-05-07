919626081f158adf2a1e89e0f63a7da17c98d293
Kanin-020 <l2001jmigue@hotmail.com>
2023-05-07 11:17:29 -0600

    Conexión a base de datos remota

--- app/dataAccess/databaseConnection.js
+++ app/dataAccess/databaseConnection.js
@@ -1,11 +1,20 @@
+require('dotenv').config();
 const mysql = require('mysql2');
 
-const connection = mysql.createConnection(({
-    host: 'localhost',
-    user: 'Kanin',
-    password: '123',
-    database: 'agenda'
-}));
+//* Conexión local a base de datos
+// const connection = mysql.createConnection(({
+//     host: 'localhost',
+//     user: 'Kanin',
+//     password: '123',
+//     database: 'agenda'
+// }));
+
+
+//* Conexión a nube de la base de datos
+const connection = mysql.createConnection(process.env.DATABASE_URL);
+console.log('Connected to PlanetScale!');
+connection.end();
+
 
 connection.connect((error) =>{
     if(error){
