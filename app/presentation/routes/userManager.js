8a3277f2ad13ea8198600922a813d9d7d830c89d
Kanin-020 <l2001jmigue@hotmail.com>
2023-05-08 22:59:08 -0600

    Manejo de logs userManager

--- app/presentation/routes/userManager.js
+++ app/presentation/routes/userManager.js
@@ -20,15 +20,15 @@ router.post('/register', (req, res) => {
     debug.debug(`Request body: ${JSON.stringify(obfuscateSensitiveData(req.body))}`);
 
     connection.query('INSERT INTO usuarios SET ?', { username: username, email: email, password: password, lastLoginDate, createdDate: createdDate }, async (error, results) => {
-        
-        try{
-            if(error.code === 'ER_DUP_ENTRY'){
+
+        try {
+            if (error.code === 'ER_DUP_ENTRY') {
                 debug.warn(`Error de validacion: La entrada ${obfuscateSensitiveData(email)} no es válida`);
                 res.status(400).json({ error: `${email} no válido.` });
-            }else{
+            } else {
                 res.json('Registro exitoso.')
             }
-        }catch (catchError){
+        } catch (catchError) {
             logger.error(catchError.stack || catchError);
             res.status(500).json({ error: catchError });
         }
@@ -53,12 +53,12 @@ router.post('/login', async (req, res) => {
 
     connection.query(query, (error, results, fields) => {
         try {
-            if (error) {
-                logger.error(error.stack || error);
-                if(error.code === 'ER_DUP_ENTRY'){
+            if (error) {     
+                if (error.code === 'ER_DUP_ENTRY') {
                     debug.warn(`Error de validacion: El usuario con email ${obfuscateSensitiveData(email)} no existe.`);
                     res.status(400).json({ error: `El usuario con email ${email} no existe.` });
                 }
+                logger.error(error.stack || error);
                 return res.status(500).json({ error });
             }
 
@@ -95,11 +95,24 @@ router.post('/login', async (req, res) => {
 
 // Obtener datos de todos los usuarios
 router.get('/usuarios', authorizer.verificarToken, (req, res) => {
+
+    // Loggear llamada de la API en INFO
+    logger.info(`${req.method} ${req.originalUrl} - Query parameters: ${JSON.stringify(req.query)} - Headers: ${JSON.stringify(req.headers)}`);
+
+    // Loggear body de la llamada en DEBUG
+    debug.debug(`Request body: ${JSON.stringify(obfuscateSensitiveData(req.body))}`);
+
     connection.query(`SELECT * FROM usuarios`, (error, results) => {
-        if (error) {
-            res.status(500).json({ error: 'Error al obtener los usuarios' });
-        } else {
-            res.json(results);
+        try {
+            if (error) {
+                logger.error(error.stack || error);
+                res.status(500).json({ error: 'Error al obtener los usuarios' });
+            } else {
+                res.json(results);
+            }
+        } catch (catchError) {
+            logger.error(error.stack || catchError);
+            res.status(500).json({ catchError: 'Error al obtener los usuarios' });
         }
     });
 });
@@ -107,13 +120,28 @@ router.get('/usuarios', authorizer.verificarToken, (req, res) => {
 // Obtener usuario por id
 router.get('/usuarios/:id', authorizer.verificarToken, (req, res) => {
     const id = req.params.id;
+
+    // Loggear llamada de la API en INFO
+    logger.info(`${req.method} ${req.originalUrl} - Query parameters: ${JSON.stringify(req.query)} - Headers: ${JSON.stringify(req.headers)}`);
+
+    // Loggear body de la llamada en DEBUG
+    debug.debug(`Request body: ${JSON.stringify(obfuscateSensitiveData(req.body))}`);
+
     connection.query('SELECT * FROM usuarios WHERE id = ?', [id], (error, results) => {
-        if (error) {
-            res.status(500).json({ error: 'Error al obtener el usuario' });
-        } else if (results.length === 0) {
-            res.status(404).json({ error: 'Usuario no encontrado' });
-        } else {
-            res.json(results[0]);
+
+        try {
+            if (error) {
+                logger.error(error.stack || error);
+                res.status(500).json({ error: 'Error al obtener el usuario solicitado' });
+            } else if (results.length === 0) {
+                debug.warn(`Error de validacion: El usuario con el id ${id} no existe.`);
+                res.status(404).json({ error: 'Usuario no encontrado' });
+            } else {
+                res.json(results[0]);
+            }
+        } catch (catchError) {
+            logger.error(error.stack || catchError);
+            res.status(500).json({ catchError: 'Error al obtener el usuario solicitado' });
         }
     });
 });
@@ -123,14 +151,28 @@ router.put('/usuarios/:id', authorizer.verificarToken, (req, res) => {
     const id = req.params.id;
     const { username, email, password } = req.body;
     const updatedUser = { username, email, password };
+
+    // Loggear llamada de la API en INFO
+    logger.info(`${req.method} ${req.originalUrl} - Query parameters: ${JSON.stringify(req.query)} - Headers: ${JSON.stringify(req.headers)}`);
+
+    // Loggear body de la llamada en DEBUG
+    debug.debug(`Request body: ${JSON.stringify(obfuscateSensitiveData(req.body))}`);
+
     connection.query('UPDATE usuarios SET ? WHERE id = ?', [updatedUser, id], (error, result) => {
-        if (error) {
-            res.status(500).json({ error: 'Error al actualizar el usuario' });
-        } else if (result.affectedRows === 0) {
-            res.status(404).json({ error: 'Usuario no encontrado' });
-        } else {
-            updatedUser.id = id;
-            res.json(updatedUser);
+        try {
+            if (error) {
+                logger.error(error.stack || error);
+                res.status(500).json({ error: 'Error al actualizar el usuario' });
+            } else if (result.affectedRows === 0) {
+                debug.warn(`Error de validacion: El usuario con el id ${id} no existe.`);
+                res.status(404).json({ error: 'Usuario no encontrado' });
+            } else {
+                updatedUser.id = id;
+                res.json(updatedUser);
+            }
+        } catch (catchError) {
+            logger.error(error.stack || catchError);
+            res.status(500).json({ catchError: 'Error al obtener el usuario solicitado' });
         }
     });
 });
@@ -138,13 +180,27 @@ router.put('/usuarios/:id', authorizer.verificarToken, (req, res) => {
 // Eliminar usuario
 router.delete('/usuarios/:id', authorizer.verificarToken, (req, res) => {
     const id = req.params.id;
+
+    // Loggear llamada de la API en INFO
+    logger.info(`${req.method} ${req.originalUrl} - Query parameters: ${JSON.stringify(req.query)} - Headers: ${JSON.stringify(req.headers)}`);
+
+    // Loggear body de la llamada en DEBUG
+    debug.debug(`Request body: ${JSON.stringify(obfuscateSensitiveData(req.body))}`);
+
     connection.query('DELETE FROM usuarios WHERE id = ?', [id], (error, result) => {
-        if (error) {
-            res.status(500).json({ error: 'Error al eliminar el usuario' });
-        } else if (result.affectedRows === 0) {
-            res.status(404).json({ error: 'Usuario no encontrado' });
-        } else {
-            res.sendStatus(204);
+        try {
+            if (error) {
+                logger.error(error.stack || error);
+                res.status(500).json({ error: 'Error al eliminar el usuario' });
+            } else if (result.affectedRows === 0) {
+                debug.warn(`Error de validacion: El usuario con el id ${id} no existe.`);
+                res.status(404).json({ error: 'Usuario no encontrado' });
+            } else {
+                res.sendStatus(204);
+            }
+        } catch (catchError) {
+            logger.error(error.stack || catchError);
+            res.status(500).json({ catchError: 'Error al obtener el usuario solicitado' });
         }
     });
 });
