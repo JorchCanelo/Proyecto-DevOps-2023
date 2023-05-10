fe4e30dd95c27757e92bd7e83f3fa3ab7a26a8b2
Kanin-020 <l2001jmigue@hotmail.com>
2023-05-09 20:05:39 -0600

    Cambio a local

--- app/dataAccess/databaseConnection.js
+++ app/dataAccess/databaseConnection.js
@@ -2,15 +2,15 @@ require('dotenv').config();
 const mysql = require('mysql2');
 
 //* Conexión local a base de datos
-// const connection = mysql.createConnection(({
-//     host: 'localhost',
-//     user: 'Kanin',
-//     password: '123',
-//     database: 'agenda'
-// }));
+const connection = mysql.createConnection(({
+    host: 'localhost',
+    user: 'Kanin',
+    password: '123',
+    database: 'agenda'
+}));
 
 //* Conexión a nube de la base de datos tipo nodejs
-const connection = mysql.createConnection(process.env.DATABASE_URL);
+//const connection = mysql.createConnection(process.env.DATABASE_URL);
 
 connection.connect((error) => {
     if (error) {
